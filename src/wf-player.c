b23c4da88c1656e680768b81013514ea72bb4dbb
Dilnavas Roshan <dilnavasroshan@gmail.com>
2025-05-01 12:56:00 +0530

    Player implementation now use GstPlay instead of directly using playbin.

--- src/wf-player.c
+++ src/wf-player.c
@@ -20,6 +20,7 @@
  */
 
 #include <gst/gst.h>
+#include <gst/play/play.h>
 
 #include "wf-player.h"
 
@@ -27,18 +28,43 @@ struct _WfPlayer
 {
     GObject parent;
 
-    GstElement *playbin;
+    GstPlay *play;
+    GstPlaySignalAdapter *signal_adaptor;
     GstBus *bus;
 };
+
+enum
+{
+    POSITION_CHNAGED,
+    N_SIGNALS
+};
+
+static guint wf_player_signals[N_SIGNALS] = {0};
 
 static void wf_player_dispose  (GObject *object);
 static void wf_player_finalize (GObject *object);
 
 /* Bus callbacks */
 
-static void bus_error_cb (GstBus     *bus,
-                          GstMessage *msg,
-                          gpointer    user_data);
+static void position_updated_cb   (WfPlayer *self,
+                                   guint64   pos,
+                                   gpointer  user_data);
+
+static void state_changed_cb      (WfPlayer     *self,
+                                   GstPlayState *state,
+                                   gpointer      user_data);
+
+static void end_of_stream_cb      (WfPlayer *self,
+                                   gpointer  user_data);
+
+static void error_cb              (WfPlayer     *self,
+                                   GError       *error,
+                                   GstStructure *details,
+                                   gpointer      user_data);
+
+static void media_info_updated_cb (WfPlayer         *self,
+                                   GstPlayMediaInfo *media_info,
+                                   gpointer          user_data);
 
 G_DEFINE_FINAL_TYPE (WfPlayer, wf_player, G_TYPE_OBJECT)
 
@@ -49,25 +75,46 @@ wf_player_class_init (WfPlayerClass *klass)
 
     object_class->dispose = wf_player_dispose;
     object_class->finalize = wf_player_finalize;
+
+    wf_player_signals[POSITION_CHNAGED] =
+        g_signal_new ("position-changed",
+                      G_TYPE_FROM_CLASS (klass),
+                      G_SIGNAL_RUN_LAST,
+                      0, NULL, NULL, NULL,
+                      G_TYPE_NONE, 1, G_TYPE_DOUBLE);
 }
 
 static void
 wf_player_init (WfPlayer *self)
 {
-    self->playbin = gst_element_factory_make ("playbin", "playbin");
-    self->bus = gst_element_get_bus (self->playbin);
-    g_signal_connect (self->bus, "message::error", G_CALLBACK (bus_error_cb), self);
-    gst_bus_add_signal_watch (self->bus);
+    self->play = gst_play_new (NULL);
+    self->signal_adaptor = gst_play_signal_adapter_new (self->play);
+
+    g_signal_connect_swapped (self->signal_adaptor, "position-updated",
+                              G_CALLBACK (position_updated_cb), self);
+    g_signal_connect_swapped (self->signal_adaptor, "state-changed",
+                              G_CALLBACK (state_changed_cb), self);
+    g_signal_connect_swapped (self->signal_adaptor, "end-of-stream",
+                              G_CALLBACK (end_of_stream_cb), self);
+    g_signal_connect_swapped (self->signal_adaptor, "media-info-updated",
+                              G_CALLBACK (media_info_updated_cb), self);
+    g_signal_connect_swapped (self->signal_adaptor, "error",
+                              G_CALLBACK (error_cb), self);
 }
 
 static void
 wf_player_dispose (GObject *object)
 {
     WfPlayer *player = WF_PLAYER (object);
+    GstBus *bus;
 
-    gst_object_unref (player->playbin);
-    gst_bus_remove_signal_watch (player->bus);
-    gst_object_unref (player->bus);
+    bus = gst_play_get_message_bus (player->play);
+    gst_bus_set_flushing (bus, TRUE);
+    gst_object_unref (bus);
+
+    g_clear_object (&player->signal_adaptor);
+    g_clear_object (&player->play);
+
     G_OBJECT_CLASS (wf_player_parent_class)->dispose (object);
 }
 
@@ -84,15 +131,45 @@ wf_player_new (void)
 }
 
 static void
-bus_error_cb (GstBus     *bus,
-              GstMessage *msg,
-              gpointer    user_data)
+position_updated_cb (WfPlayer *self,
+                     guint64   pos,
+                     gpointer  user_data)
 {
-    GError *error = NULL;
-    gchar *debug_info;
+    guint64 duration;
+    gdouble pos_frac;
 
-    gst_message_parse_error (msg, &error, &debug_info);
-    g_printerr ("Error: %s\n", debug_info);
+    duration = gst_play_get_duration (self->play);
+    pos_frac = pos / (gdouble) duration;
+    g_signal_emit (self, wf_player_signals[POSITION_CHNAGED], 0, pos_frac);
+}
+
+static void
+state_changed_cb (WfPlayer     *self,
+                  GstPlayState *state,
+                  gpointer      user_data)
+{
+}
+
+static void
+end_of_stream_cb (WfPlayer *self,
+                  gpointer  user_data)
+{
+}
+
+static void
+error_cb (WfPlayer     *self,
+          GError       *error,
+          GstStructure *details,
+          gpointer      user_data)
+{
+    g_printerr ("%s\n", error->message);
+}
+
+static void
+media_info_updated_cb (WfPlayer         *self,
+                       GstPlayMediaInfo *media_info,
+                       gpointer          user_data)
+{
 }
 
 void
@@ -101,19 +178,12 @@ wf_player_set_file (WfPlayer    *self,
 {
     g_return_if_fail (WF_IS_PLAYER (self));
 
-    g_object_set (self->playbin, "uri", uri, NULL);
+    g_object_set (self->play, "uri", uri, NULL);
 }
 
 void
 wf_player_play (WfPlayer *self)
 {
-    int status;
-
-    g_return_if_fail (WF_IS_PLAYER (self));
-
-    status = gst_element_set_state (self->playbin, GST_STATE_PLAYING);
-    if (status == GST_STATE_CHANGE_FAILURE) {
-        g_printerr ("Error: state change failure\n");
-    }
+    gst_play_play (self->play);
 }
 
